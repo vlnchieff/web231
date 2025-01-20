20dd85fef40506165e97f922994e614e760870cb
Chieff Weaver <vlnchieff>
2025-01-20 00:39:36 -0500

    Ch.2 Project work

--- Week-2/project/project02-02_txt.js
+++ Week-2/project/project02-02_txt.js
@@ -3,9 +3,24 @@
       Project 02-02
 
       Application to test for completed form
-      Author: 
-      Date:   
+      Author: Chris Weaver
+      Date:   January 17, 2025
 
       Filename: project02-02.js
- */
- 
+*/
+
+
+function verifyForm() {
+    // Variables to store form data
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+
+
+    // Operator to check if all fields are filled in
+    (name && email && phone) ? window.alert("Thank You!") : window.alert("Please Fill in All Fields");
+
+}
+
+// Event listener to call verifyForm function
+document.getElementById("submit").addEventListener("click", verifyForm);
