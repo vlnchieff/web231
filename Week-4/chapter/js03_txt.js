c2ff72415709132f2b5d6babd9f0244f3de9f170
Chieff Weaver <vlnchieff>
2025-02-03 00:19:56 -0500

    Week 3 Adjustments & Week 4 Work

---
+++ Week-4/chapter/js03_txt.js
@@ -0,0 +1,15 @@
+/*    JavaScript 7th Edition
+     Chapter 3
+     Chapter case
+
+     Tipton Turbines
+     Program to display games results in a web table
+     Author: Chris Weaver
+     Date:   Jan 31 2025
+
+     Filename: js03.js
+*/
+
+// Days of the week
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+document.write("weekDays");
