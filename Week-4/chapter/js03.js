d9b31d7edc9848a268701a25f2f3c6e22851fab8
Chieff Weaver <vlnchieff>
2025-02-09 03:29:26 -0500

    week 5

--- Week-4/chapter/js03.js
+++ Week-4/chapter/js03.js
@@ -24,3 +24,41 @@ function addWeekdays() {
     i++;
   }
 }
+
+window.addEventListener("load", showGames);
+
+function showGames() {
+  for (let i = 0; i < gameDates.length; i++){
+    let gameInfo = "";
+    switch (gameResults[i]){
+      case "W":
+        gameInfo += "<p class='win'>";
+        break;
+      case "L":
+        gameInfo += "<p class='lose'>";
+        break;
+      case "S":
+        gameInfo += "<p class='suspended'>";
+        break;
+      case "P":
+        gameInfo += "<p class='postponed'>";
+        break;
+    }
+
+    //Open a paragraph tag
+    gameInfo += "<p>";
+
+    //Include the opponent
+    gameInfo += gameOpponents[i] + "<br>";
+
+    //Include the result and score
+    gameInfo += gameResults[i] + ": (" + runsScored[i] + "-" + runsAllowed[i] + ")";
+
+    //Close the paragraph tag
+    gameInfo += "</p>";
+
+    //write the information into a table cell
+    let tableCell = document.getElementById(gameDates[i]);
+    tableCell.insertAdjacentHTML("beforeEnd", gameInfo);
+  }
+}
